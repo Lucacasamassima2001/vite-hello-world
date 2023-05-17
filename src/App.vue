9a2b48b4576eab80fb09810adb24f56295d9d825
Lucacasamassima2001 <casamassimaluca723@gmail.com>
2023-05-17 16:47:50 +0200

    ex done

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import ComponentTitle from './components/ComponentTitle.vue'
+</script>
+
+<template>
+  <ComponentTitle/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ComponentTitle.vue
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<template>
+  <h1>LA MIA PRIMA APP CON VITE!</h1>
+</template>
+
+<style scoped>
+h1{
+  text-align: center;
+  margin-top: 200px;
+  font-size: 4em;
+  color: rgb(45, 45, 147);
+}
+</style>
